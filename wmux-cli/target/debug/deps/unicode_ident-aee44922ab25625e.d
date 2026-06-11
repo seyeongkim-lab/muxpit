dcf9ef23ca4d8f10cec9b10e079d32fcff6930a1
Seyeong Kim <me@seyeong.kim>
2026-06-11 14:02:09 +0900

    Stop tracking wmux-cli build artifacts
    Signed-off-by: Seyeong Kim <master@seyeong.kim>

--- wmux-cli/target/debug/deps/unicode_ident-aee44922ab25625e.d
+++
@@ -1,8 +0,0 @@
-C:\Users\one\projects\wmux\wmux-cli\target\debug\deps\unicode_ident-aee44922ab25625e.d: C:\Users\one\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\lib.rs C:\Users\one\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\tables.rs
-
-C:\Users\one\projects\wmux\wmux-cli\target\debug\deps\libunicode_ident-aee44922ab25625e.rlib: C:\Users\one\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\lib.rs C:\Users\one\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\tables.rs
-
-C:\Users\one\projects\wmux\wmux-cli\target\debug\deps\libunicode_ident-aee44922ab25625e.rmeta: C:\Users\one\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\lib.rs C:\Users\one\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\tables.rs
-
-C:\Users\one\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\lib.rs:
-C:\Users\one\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\tables.rs:
--- wmux-cli/target/debug/deps/zmij-ad706ccbdeeacfe2.d
+++
@@ -1,7 +0,0 @@
-C:\Users\one\Projects\wmux\wmux-cli\target\debug\deps\zmij-ad706ccbdeeacfe2.d: C:\Users\one\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\lib.rs C:\Users\one\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\stdarch_x86.rs C:\Users\one\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\traits.rs
-
-C:\Users\one\Projects\wmux\wmux-cli\target\debug\deps\libzmij-ad706ccbdeeacfe2.rmeta: C:\Users\one\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\lib.rs C:\Users\one\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\stdarch_x86.rs C:\Users\one\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\traits.rs
-
-C:\Users\one\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\lib.rs:
-C:\Users\one\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\stdarch_x86.rs:
-C:\Users\one\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\zmij-1.0.21\src\traits.rs:
