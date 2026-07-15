f1a6353dd88cb5f7211ba8c4ee09a31787a4e892
Seyeong Kim <me@seyeong.kim>
2026-07-16 03:06:38 +0900

    Release v0.2.5
    Signed-off-by: Seyeong Kim <me@seyeong.kim>

--- src/mobile/codexMobileClient.ts
+++ src/mobile/codexMobileClient.ts
@@ -35,7 +35,7 @@ export class CodexMobileClient {
       clientInfo: {
         name: "wmux_mobile",
         title: "wmux Mobile",
-        version: "0.2.4",
+        version: "0.2.5",
       },
     });
     await this.notify("initialized", {});
